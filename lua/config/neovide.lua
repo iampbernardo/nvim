75ae1596541e8ad3644f6b3f42908ff24d4676b5
Pablo Bernardo Baragaño <iam@pbernardo.com>
2023-05-05 23:54:22 +0200

    Add configurations for neovide

---
+++ lua/config/neovide.lua
@@ -0,0 +1,7 @@
+-- Neovide is the GUI I use for Neovim
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+
+  vim.opt.linespace = 0
+  vim.g.neovide_hide_mouse_when_typing = true
+end
